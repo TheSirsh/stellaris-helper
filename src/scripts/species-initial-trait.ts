8a3d0b83715a7eb25668c52454b30785e2301d32
TheSirsh <sirsh@bk.ru>
2022-12-13 20:37:15 +0300

    feat: add initial biological species traits list and images

---
+++ src/scripts/species-initial-trait.ts
@@ -0,0 +1,236 @@
+const speciesBiologicalTraits: object = {
+  "0": {
+    "trait": "trait_adaptive",
+    "icon": "../src/icons/traits-spicies-initial/adaptive.png",
+    "nameEN": "Adaptive",
+    "nameRU": "",
+    "number1": "+10%",
+    "text1EN": "Habitability",
+    "text1RU": ""
+  },
+  "1": {
+    "trait": "trait_extremely_adaptive",
+    "icon": "../src/icons/traits-spicies-initial/extremely-adaptive.png",
+    "nameEN": "Extremely Adaptive",
+    "nameRU": "",
+    "number1": "+20%",
+    "text1EN": "Habitability",
+    "text1RU": ""
+  },
+  "2": {
+    "trait": "trait_agrarian",
+    "icon": "../src/icons/traits-spicies-initial/agrarian.png",
+    "nameEN": "Agrarian",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Food from Jobs",
+    "text1RU": ""
+  },
+  "3": {
+    "trait": "trait_charismatic",
+    "icon": "../src/icons/traits-spicies-initial/charismatic.png",
+    "nameEN": "Charismatic",
+    "nameRU": "",
+    "number1": "+20%",
+    "text1EN": "Amenities from Jobs",
+    "text1RU": ""
+  },
+  "4": {
+    "trait": "trait_communal",
+    "icon": "../src/icons/traits-spicies-initial/communal.png",
+    "nameEN": "Communal",
+    "nameRU": "",
+    "number1": "-10%",
+    "text1EN": "Pop Housing Usage",
+    "text1RU": ""
+  },
+  "5": {
+    "trait": "trait_conformists",
+    "icon": "../src/icons/traits-spicies-initial/conformists.png",
+    "nameEN": "Conformists",
+    "nameRU": "",
+    "number1": "+30%",
+    "text1EN": "Governing Ethics Attraction",
+    "text1RU": ""
+  },
+  "6": {
+    "trait": "trait_conservational",
+    "icon": "../src/icons/traits-spicies-initial/conservationalists.png",
+    "nameEN": "Conservationist",
+    "nameRU": "",
+    "number1": "−10%",
+    "text1EN": "Pop consumer goods upkeep",
+    "text1RU": ""
+  },
+  "7": {
+    "trait": "trait_docile",
+    "icon": "../src/icons/traits-spicies-initial/docile.png",
+    "nameEN": "Docile",
+    "nameRU": "",
+    "number1": "−10%",
+    "text1EN": "Empire Size from Pops",
+    "text1RU": ""
+  },
+  "8": {
+    "trait": "trait_enduring",
+    "icon": "../src/icons/traits-spicies-initial/enduring.png",
+    "nameEN": "Enduring",
+    "nameRU": "",
+    "number1": "+20",
+    "text1EN": "years Leader Lifespan",
+    "text1RU": ""
+  },
+  "9": {
+    "trait": "trait_venerable",
+    "icon": "../src/icons/traits-spicies-initial/venerable.png",
+    "nameEN": "Venerable",
+    "nameRU": "",
+    "number1": "+80",
+    "text1EN": "years Leader Lifespan",
+    "text1RU": ""
+  },
+  "10": {
+    "trait": "trait_industrious",
+    "icon": "../src/icons/traits-spicies-initial/industrious.png",
+    "nameEN": "Industrious",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Minerals from Jobs",
+    "text1RU": ""
+  },
+  "11": {
+    "trait": "trait_ingenious",
+    "icon": "../src/icons/traits-spicies-initial/ingenious.png",
+    "nameEN": "Ingenious",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Energy Credits from Jobs",
+    "text1RU": ""
+  },
+  "12": {
+    "trait": "trait_intelligent",
+    "icon": "../src/icons/traits-spicies-initial/intelligent.png",
+    "nameEN": "Intelligent",
+    "nameRU": "",
+    "number1": "+10%",
+    "text1EN": "Research from Jobs",
+    "text1RU": ""
+  },
+  "13": {
+    "trait": "trait_natural_engineers",
+    "icon": "../src/icons/traits-spicies-initial/natural-engineers.png",
+    "nameEN": "Natural Engineers",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Engineering Research from Jobs",
+    "text1RU": ""
+  },
+  "14": {
+    "trait": "trait_natural_physicists",
+    "icon": "../src/icons/traits-spicies-initial/natural-physicists.png",
+    "nameEN": "Natural Physicists",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Physics Research from Jobs",
+    "text1RU": ""
+  },
+  "15": {
+    "trait": "trait_natural_sociologists",
+    "icon": "../src/icons/traits-spicies-initial/natural-sociologists.png",
+    "nameEN": "Natural Sociologists",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Society Research from Jobs",
+    "text1RU": ""
+  },
+  "16": {
+    "trait": "trait_nomadic",
+    "icon": "../src/icons/traits-spicies-initial/nomadic.png",
+    "nameEN": "Nomadic",
+    "nameRU": "",
+    "number1": "+15%",
+    "text1EN": "Pop growth from immigration",
+    "text1RU": "",
+    "number2": "-25%",
+    "text2EN": "Resettlement Cost",
+    "text2RU": "",
+  },
+  "17": {
+    "trait": "trait_quick_learners",
+    "icon": "../src/icons/traits-spicies-initial/quick-learners.png",
+    "nameEN": "Quick Learners",
+    "nameRU": "",
+    "number1": "+25%",
+    "text1EN": "Leader Experience Gain",
+    "text1RU": ""
+  },
+  "18": {
+    "trait": "trait_rapid_breeders",
+    "icon": "../src/icons/traits-spicies-initial/rapid-breeders.png",
+    "nameEN": "Rapid Breeders",
+    "nameRU": "",
+    "number1": "+10%",
+    "text1EN": "Growth Speed",
+    "text1RU": ""
+  },
+  "19": {
+    "trait": "trait_resilient",
+    "icon": "../src/icons/traits-spicies-initial/resilient.png",
+    "nameEN": "Resilient",
+    "nameRU": "",
+    "number1": "+50%",
+    "text1EN": "Defense Army Damage",
+    "text1RU": ""
+  },
+  "20": {
+    "trait": "trait_strong",
+    "icon": "../src/icons/traits-spicies-initial/strong.png",
+    "nameEN": "Strong",
+    "nameRU": "",
+    "number1": "+20%",
+    "text1EN": "Army Damage",
+    "text1RU": "",
+    "number2": "+2.5%",
+    "text2EN": "Worker and Menial Drone Pop Resource Output",
+    "text2RU": "",
+  },
+  "21": {
+    "trait": "trait_very_strong",
+    "icon": "../src/icons/traits-spicies-initial/very-strong.png",
+    "nameEN": "Very Strong",
+    "nameRU": "",
+    "number1": "+40%",
+    "text1EN": "Army Damage",
+    "text1RU": "",
+    "number2": "+5%",
+    "text2EN": "Worker and Menial Drone Pop Resource Output",
+    "text2RU": "",
+  },
+  "22": {
+    "trait": "trait_talented",
+    "icon": "../src/icons/traits-spicies-initial/talented.png",
+    "nameEN": "Talented",
+    "nameRU": "",
+    "number1": "+1",
+    "text1EN": "Leader Level Cap",
+    "text1RU": ""
+  },
+  "23": {
+    "trait": "trait_thrifty",
+    "icon": "../src/icons/traits-spicies-initial/thrifty.png",
+    "nameEN": "Thrifty",
+    "nameRU": "",
+    "number1": "+25%",
+    "text1EN": "Trade value from Jobs",
+    "text1RU": ""
+  },
+  "24": {
+    "trait": "trait_traditional",
+    "icon": "../src/icons/traits-spicies-initial/traditional.png",
+    "nameEN": "Traditional",
+    "nameRU": "",
+    "number1": "+10%",
+    "text1EN": "Unity from Jobs",
+    "text1RU": ""
+  }
+}
