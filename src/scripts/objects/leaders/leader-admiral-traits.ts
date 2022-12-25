92cbdb24f5c941c8ca68622b6eb82995c1b4ab6d
TheSirsh <sirsh@bk.ru>
2022-12-25 19:36:49 +0300

    feat: add syth admiral traits

--- src/scripts/objects/leaders/leader-admiral-traits.ts
+++ src/scripts/objects/leaders/leader-admiral-traits.ts
@@ -279,7 +279,7 @@ const leaderAdmiralBiological: Array<ITraits> = [
     nameEN: "Fleet Logistician",
     nameRU: "",
     traits: [
-      ["good", "", "Immortal",],
+      ["good", "-10%", "Ship Upkeep",],
     ],
   },
   {
@@ -542,4 +542,298 @@ const leaderAdmiralBiological: Array<ITraits> = [
   },
 ]
 
-export { leaderAdmiralBiological, }
+const leaderAdmiralSynthitic: Array<ITraits> = [
+  {
+    trait: "leader_trait_admiral_synthetic",
+    icon: "../src/icons/leader-traits/psionic.png",
+    nameEN: "Psychic",
+    nameRU: "",
+    traits: [
+      ["good", "+10%", "Ship weapons damage",],
+      ["good", "+10%", "Shield hardening",],
+    ],
+    exclude: [
+      "leader_trait_admiral_cyborg",
+      "leader_trait_admiral_psionic",
+      "leader_trait_admiral_chosen",
+      "leader_trait_admiral_instrument_chosen",
+      "leader_trait_admiral_eater_chosen",
+      "leader_trait_admiral_whisperers_chosen",
+      "leader_trait_hive_mind",
+    ],
+  },
+  {
+    trait: "leader_trait_aggressive",
+    icon: "../src/icons/leader-traits/admiral-aggressive.png",
+    nameEN: "Aggressive",
+    nameRU: "",
+    traits: [
+      ["good", "+5%", "Fire Rate",],
+      ["good", "+10%", "Sublight Speed",],
+    ],
+    exclude: [
+      "leader_trait_cautious",
+    ],
+  },
+  {
+    trait: "leader_trait_cautious",
+    icon: "../src/icons/leader-traits/admiral-cautious.png",
+    nameEN: "Cautious",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Weapons Range",],
+    ],
+    exclude: [
+      "leader_trait_aggressive",
+    ],
+  },
+  {
+    trait: "leader_trait_engineer",
+    icon: "../src/icons/leader-traits/admiral-engineer.png",
+    nameEN: "Engineer",
+    nameRU: "",
+    traits: [
+      ["good", "+0.5%", "Daily Hull Regeneration",],
+      ["good", "+0.5%", "Daily Armor Regeneration",],
+    ],
+  },
+  {
+    trait: "leader_trait_maintenance_loop",
+    icon: "../src/icons/leader-traits/admiral-maintenance-loop.png",
+    nameEN: "Maintenance Loop",
+    nameRU: "",
+    traits: [
+      ["good", "-10%", "Ship Upkeep",],
+    ],
+  },
+  {
+    trait: "leader_trait_gale_speed",
+    icon: "../src/icons/leader-traits/admiral-gale-speed.png",
+    nameEN: "Gale Speed",
+    nameRU: "",
+    traits: [
+      ["good", "-10%", "Ship Upkeep",],
+    ],
+    exclude: [
+      "leader_trait_lethargic",
+    ],
+  },
+  {
+    trait: "leader_trait_scout",
+    icon: "../src/icons/leader-traits/admiral-scout.png",
+    nameEN: "Scout",
+    nameRU: "",
+    traits: [
+      ["good", "+1", "Sensor Range",],
+      ["good", "+2", "Hyperlane Detection Range",],
+      ["good", "+10%", "Sublight Speed",],
+    ],
+  },
+  {
+    trait: "leader_trait_trickster",
+    icon: "../src/icons/leader-traits/admiral-trickster.png",
+    nameEN: "Trickster",
+    nameRU: "",
+    traits: [
+      ["good", "-15%", "Emergency FTL Damage Risk",],
+      ["good", "+25%", "Combat Disengagement Chance",],
+    ],
+    exclude: [
+      "leader_trait_unyielding",
+    ],
+  },
+  {
+    trait: "leader_trait_unyielding",
+    icon: "../src/icons/leader-traits/admiral-unyielding.png",
+    nameEN: "Unyielding",
+    nameRU: "",
+    traits: [
+      ["good", "+10%", "Ship Hull Points",],
+      ["good", "+5%", "Ship weapons damage",],
+      ["bad", "-33%", "Combat Disengagement Chance",],
+    ],
+    exclude: [
+      "leader_trait_nervous",
+      "leader_trait_trickster",
+    ],
+  },
+  {
+    trait: "leader_trait_unstable_code_base",
+    icon: "../src/icons/leader-traits/admiral-unstable-code-base.png",
+    nameEN: "Unstable Code Base",
+    nameRU: "",
+    traits: [
+      ["bad", "-10%", "Fire Rate",],
+    ],
+    exclude: [
+      "leader_trait_unyielding",
+    ],
+  },
+  {
+    trait: "leader_trait_lethargic",
+    icon: "../src/icons/leader-traits/admiral-lethargic.png",
+    nameEN: "Lethargic",
+    nameRU: "",
+    traits: [
+      ["bad", "-5%", "Evasion",],
+      ["bad", "-10%", "Sublight Speed",],
+    ],
+    exclude:[
+      "leader_trait_gale_speed",
+    ],
+  },
+  {
+    trait: "leader_trait_cybrex",
+    icon: "../src/icons/leader-traits/admiral-cybrex-database.png",
+    nameEN: "Cybrex Databases",
+    nameRU: "",
+    traits: [
+      ["good", "+10%", "Evasion",],
+      ["good", "+10%", "Fire Rate",],
+      ["good", "+20%", "Damage to the Contingency",],
+    ],
+  },
+  {
+    trait: "leader_trait_dimensional_stutter",
+    icon: "../src/icons/leader-traits/admiral-dimensional-slutter.png",
+    nameEN: "Dimensional Stutter",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Sublight Speed",],
+    ],
+  },
+  {
+    trait: "leader_trait_ethereal",
+    icon: "../src/icons/leader-traits/admiral-ethernal.png",
+    nameEN: "Ethereal",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Evasion",],
+    ],
+  },
+  {
+    trait: "leader_trait_hive_affinity",
+    icon: "../src/icons/leader-traits/admiral-hive-affinity.png",
+    nameEN: "Hive Affinity",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Fire Rate",],
+    ],
+  },
+  {
+    trait: "leader_trait_sentinel",
+    icon: "../src/icons/leader-traits/admiral-sentinel-training.png",
+    nameEN: "Sentinel Training",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Damage to Prethoryn Creatures",],
+    ],
+  },
+  {
+    trait: "leader_trait_void_swimmer",
+    icon: "../src/icons/leader-traits/admiral-void-hunter.png",
+    nameEN: "Void Hunter",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Sublight Speed",],
+    ],
+  },
+  {
+    trait: "leader_trait_caravan_counter",
+    icon: "../src/icons/leader-traits/admiral-caravan-counter.png",
+    nameEN: "Caravan Counter",
+    nameRU: "",
+    traits: [
+      ["good", "-10%", "Emergency FTL Damage Risk",],
+    ],
+  },
+  {
+    trait: "leader_trait_dragonslayer",
+    icon: "../src/icons/leader-traits/admiral-dragonslayer.png",
+    nameEN: "Dragonslayer",
+    nameRU: "",
+    traits: [
+      ["good", "+10%", "Sublight Speed",],
+      ["good", "+10%", "Fire Rate",],
+    ],
+  },
+  {
+    trait: "leader_trait_foredoomed_to_a_rendezvous",
+    icon: "../src/icons/leader-traits/admiral-foredoomed-to-a-rendezvous.png",
+    nameEN: "Foredoomed to a Rendezvous",
+    nameRU: "",
+    traits: [
+      ["good", "+25%", "Sublight Speed",],
+    ],
+  },
+  {
+    trait: "leader_trait_bulwark_watcher",
+    icon: "../src/icons/leader-traits/admiral-bulwark-watcher.png",
+    nameEN: "Bulwark Watcher",
+    nameRU: "",
+    traits: [
+      ["good", "+15%", "Evasion in Friendly Systems",],
+      ["good", "+15%", "Tracking in Friendly Systems",],
+    ],
+  },
+  {
+    trait: "leader_trait_bulwark_retaliator",
+    icon: "../src/icons/leader-traits/admiral-bulwark-retaliator.png",
+    nameEN: "Bulwark Retaliator",
+    nameRU: "",
+    traits: [
+      ["good", "+15%", "Ship Fire Rate in Friendly Systems",],
+    ],
+  },
+  {
+    trait: "leader_trait_bulwark_sentinel",
+    icon: "../src/icons/leader-traits/admiral-bulwark-sentinel.png",
+    nameEN: "Bulwark Sentinel",
+    nameRU: "",
+    traits: [
+      ["good", "+15%", "Shield Hit Points in Friendly Systems",],
+    ],
+  },
+  {
+    trait: "leader_trait_mercenary_warrior",
+    icon: "../src/icons/leader-traits/admiral-mercenary-warrior.png",
+    nameEN: "Mercenary Warrior",
+    nameRU: "",
+    traits: [
+      ["good", "+10%", "Evasion",],
+      ["good", "+10%", "Fire Rate",],
+    ],
+  },
+  {
+    trait: "leader_trait_admiral_hells_heart",
+    icon: "../src/icons/leader-traits/admiral-hells-heart.png",
+    nameEN: "Hell's Heart",
+    nameRU: "",
+    traits: [
+      ["good", "+10%", "Fire Rate",],
+      ["good", "+10%", "Sublight Speed",],
+      ["good", "+10%", "Ship weapons damage",],
+      ["bad", "-10%", "Leader experience gain",],
+      ["bad", "-5%", "Evasion",],
+      ["bad", "-33%", "Combat Disengagement Chance",],
+    ],
+  },
+  {
+    trait: "leader_trait_great_khan",
+    icon: "../src/icons/leader-traits/admiral-great-khan.png",
+    nameEN: "The Great Khan",
+    nameRU: "",
+    traits: [
+      ["good", "+20%", "Fire Rate",],
+      ["good", "+20%", "Sublight Speed",],
+      ["good", "+10%", "Ship weapons damage",],
+      ["good", "+10%", "Ship weapons range",],
+      ["good", "+20%", "Ship Hull Points",],
+      ["good", "+30%", "Evasion",],
+      ["good", "+0.5%", "daily hull regeneration",],
+      ["good", "+0.5%", "daily armor  regeneration",],
+    ],
+  },
+]
+
+export { leaderAdmiralBiological, leaderAdmiralSynthitic }
