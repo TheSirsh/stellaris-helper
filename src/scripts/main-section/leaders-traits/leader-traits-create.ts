64ad1934eb673a9974aa0bdca63d0b85ac6212db
TheSirsh <sirsh@bk.ru>
2022-12-26 16:14:10 +0300

    feat: add create leader traits mutually exclusive script

--- src/scripts/main-section/leaders-traits/leader-traits-create.ts
+++ src/scripts/main-section/leaders-traits/leader-traits-create.ts
@@ -6,19 +6,36 @@ import { createMainSectionClimateTrait } from "../climate-traits/climate-traits-
 import { createWorkedPlace, createTraitList, createDescrBlock } from "../../create-worked-place.js";
 import { checkingLeader } from "../../checking-leader-function.js";
 import { ITraits } from "../../interface.js";
+import { selectLeaderTrait } from "./leader-traits-selector.js";
 
 function createLeaderTraits(): void {
 
-  let validArray: Array<ITraits> = checkingLeader()
+  let validTraits: Array<ITraits> = checkingLeader()
 
   createWorkedPlace("leader-traits", speciesSectionText);
   const workedplace = document.querySelector(".leader-traits__workedplace");
 
   const traitlistBlock = createNewSimpleElement("div", "leader-traits__traitlist-block", workedplace)
 
-  createTraitList(traitlistBlock, validArray, "leader-traits");
+  createTraitList(traitlistBlock, validTraits, "leader-traits");
   createDescrBlock("leader-traits", speciesSectionText);
 
+  const traitContainerArr = document.querySelectorAll(".leader-traits__trait-container");
+
+  traitContainerArr.forEach(function(elem: Element, i: number) {
+    elem.addEventListener("click", function(): void {
+      traitContainerArr[i].classList.remove("red");
+      let isActive: boolean;
+      if (traitContainerArr[i].classList.contains("leader-traits__trait-container_active")) {
+        isActive = true;
+        traitContainerArr[i].classList.remove("leader-traits__trait-container_active");
+      } else {
+        isActive = false;
+        traitContainerArr[i].classList.add("leader-traits__trait-container_active");
+      }
+      selectLeaderTrait(i, validTraits, isActive);
+    })
+  })
 }
 
 export { createLeaderTraits }
