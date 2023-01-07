6ff3d0a27060aa5f7e69b118cd3e872e1a07eee4
TheSirsh <sirsh@bk.ru>
2023-01-07 20:48:19 +0300

    feat: add next track script

--- src/scripts/audio-player/audio-player.ts
+++ src/scripts/audio-player/audio-player.ts
@@ -16,49 +16,84 @@ function createAudioPlayer() {
       createNewImageElement(`${audioPlayerText[i].name}`, audioPlayer, `${audioPlayerText[i].src}`, `${audioPlayerText[i].alt}`)
     }
 
-    let trackNumber: number = randomizer(0, trackList.length - 1)
+    let trackNumber: number = randomizer(0, trackList.length - 1);
+    localStorage.setItem("trackNumber", trackNumber.toString())
     let audioTrack = new Audio(trackList[trackNumber].src)
     let duration = convertTime(trackList[trackNumber].duration)
     createNewTextElement("span", "duration", audioPlayer, "00:00 / " + duration);
-    createNewTextElement("span", "trackName", audioPlayer, trackList[trackNumber].name);
+    createNewTextElement("span", "track-name", audioPlayer, trackList[trackNumber].name);
 
-    const playButton = document.querySelector(".play");
+    let playButton = document.querySelector(".play");
     localStorage.setItem("trackTime", "0")
-    playButton.addEventListener("click", function() {
+    
+    playButton.addEventListener("click", () => {
       tooglePlay(isPlay, audioTrack, trackNumber)
       isPlay = !isPlay
+    }
+    )
+
+    const nextButton = document.querySelector(".next")
+    nextButton.addEventListener("click", function() {
+      nextTrack(audioTrack, isPlay)
+      isPlay = true
     })
 }
 
-function tooglePlay(playStatus: boolean, track: HTMLAudioElement, trackNumber: number) {
+function tooglePlay(playStatus: boolean, track: HTMLAudioElement, trackNumber2: number) {
   let button: HTMLImageElement = document.querySelector(".play");
-  let time = parseFloat(localStorage.getItem("trackTime"))
-  if (playStatus === false) {
-    track.currentTime = time;
-    track.play();
-    button.src = "../src/icons/audio-player/pause.png";
-    let player = setInterval(function() {
-      time++;
-      updateTime(time, trackNumber);
-      button.addEventListener("click", function() {
-        clearInterval(player);
-      })
-    }, 1000)
+  let time = parseFloat(localStorage.getItem("trackTime"));
+  let trackNumber = parseInt(localStorage.getItem("trackNumber"))
 
-  } else if (playStatus === true) {
+  if (playStatus === true) {
     button.src = "../src/icons/audio-player/play.png";
     track.pause();
     localStorage.setItem("trackTime", track.currentTime.toString())
+  } else if (playStatus === false) {
+    button.src = "../src/icons/audio-player/pause.png";
+    track.currentTime = time;
+    track.play();
+
+    let duration = document.querySelector(".duration");
+    duration.textContent =  "00:00 / " + convertTime(trackList[trackNumber].duration);
+    let trackName = document.querySelector(".track-name");
+    trackName.textContent = trackList[trackNumber].name;
+
+    setInterval(function() {
+      updateTime(track)
+    }, 1000)
+  
   }
+
+
+
+
 }
 
-function updateTime(time: number, trackNumber: number) {
+function nextTrack(track: HTMLAudioElement, playStatus: boolean) {
+  if (!playStatus) {
+    let button: HTMLImageElement = document.querySelector(".play");
+    button.src = "../src/icons/audio-player/play.png";
+  }
+
+  localStorage.setItem("trackTime", "0");
+  let trackNumber = parseInt(localStorage.getItem("trackNumber"))
+  if (trackNumber ===  trackList.length - 1) { trackNumber = 0; }
+    else { trackNumber++; }
+  localStorage.setItem("stopTimer", "yes")
+  track.src = trackList[trackNumber].src;
+  updateTime(track);
+  localStorage.setItem("trackNumber", trackNumber.toString())
+  tooglePlay(false, track, trackNumber)
+}
+
+function updateTime(track: HTMLAudioElement) {
+  let time = track.currentTime;
+  
+  let trackNumber = parseInt(localStorage.getItem("trackNumber"))
   let duration = document.querySelector(".duration");
   duration.textContent = convertTime(time) + " / " + convertTime(trackList[trackNumber].duration);
-
   let progressBar: HTMLImageElement = document.querySelector(".progress-bar");
   progressBar.style.width = (time * 100) / trackList[trackNumber].duration + "%"
-
 }
 
 export { createAudioPlayer }
