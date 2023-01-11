8069e219cf14a9d38a8ce92120d7167188ea0654
TheSirsh <sirsh@bk.ru>
2023-01-11 21:34:32 +0300

    feat: add completed leader script

--- dist/src/scripts/main-section/leaders-traits/leader-traits-completed.js
+++
@@ -1,3 +0,0 @@
-function completeLeaderTrait() {
-}
-export { completeLeaderTrait };
--- src/scripts/main-section/leaders-traits/leader-traits-completed.ts
+++ src/scripts/main-section/leaders-traits/leader-traits-completed.ts
@@ -1,5 +1,41 @@
-function completeLeaderTrait() {
+import { ITraits } from "../../interface.js";
+import { createWorkedPlace, createTraitList, createDescrBlock } from "../../create-worked-place.js";
+import { createNewTextElement } from "../../create-functions.js";
+import { createCodeBlock } from "../../create-code-block.js";
+import { leaderSectionText } from "./leader-section-text.js";
+import { summarizeArray } from "../../summarize-array.js";
 
+function completeLeaderTrait() {
+  let resultArray: Array<ITraits> = [];
+  let arr = JSON.parse(localStorage.getItem("traits"));
+  resultArray = resultArray.concat(arr);
+
+  createWorkedPlace("completed", leaderSectionText);
+  createTraitList(document.querySelector(".completed__workedplace"), resultArray, "completed");
+  createDescrBlock("completed", leaderSectionText);
+    document.querySelector(".completed__button").remove();
+
+  createCodeBlock("completed", "Stellaris Inner Code", "traits", resultArray);
+
+  const textBlock = document.querySelector(".completed__descr-text");
+    textBlock.textContent = "";
+
+  let traitList: Array<Array<string>> = [];
+    for (let i = 0; i < resultArray.length; i++) {
+      for (let j = 0; j < resultArray[i].traits.length; j++) {
+        traitList.push(resultArray[i].traits[j])
+      }
+    }
+
+  const reducedTraitList = summarizeArray(traitList)
+
+  for (let i = 0; i < reducedTraitList.length; i++) {
+    if (reducedTraitList[i]) {
+        if (reducedTraitList[i][0] === "bad") { createNewTextElement("span", "red", textBlock, reducedTraitList[i][1]); } 
+          else if (reducedTraitList[i][0] === "good") { createNewTextElement("span", "green", textBlock, reducedTraitList[i][1]); }
+        textBlock.innerHTML += " " + reducedTraitList[i][2] + "<br>";
+    }
+  }
 }
 
 export{ completeLeaderTrait }
